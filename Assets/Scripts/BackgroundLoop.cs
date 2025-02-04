70a8da9472b9d16eda5571192d96b3004e261bc3
yeonjin <xxyeon17@gmail.com>
2025-02-04 17:44:49 +0900

    전체 파일 커밋

---
+++ Assets/Scripts/BackgroundLoop.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+// 왼쪽 끝으로 이동한 배경을 오른쪽 끝으로 재배치하는 스크립트
+public class BackgroundLoop : MonoBehaviour {
+    private float width; // 배경의 가로 길이
+
+    // 가로 길이를 측정하는 처리
+    private void Awake() {
+        BoxCollider2D backgroundCollider = GetComponent<BoxCollider2D>();
+        width = backgroundCollider.size.x;
+    }
+
+    // 현재 위치가 원점에서 왼쪽으로 width 이상 이동했을때 위치를 리셋
+    private void Update() {
+        if (-width >= transform.position.x)
+            Reposition();
+    }
+
+    // 위치를 리셋하는 메서드
+    private void Reposition() {
+        transform.position += new Vector3(width * 2f, 0, 0);
+    }
+}
